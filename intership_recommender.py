9d5de6916fe010e6811999605721c51226822f5d
Shubham Bhatia <shubhambhatia534@gmail.com>
2025-11-02 02:37:31 +0530

    Minor changes

--- intership_recommender.py
+++ intership_recommender.py
@@ -38,7 +38,7 @@ def main():
     nltk.download('averaged_perceptron_tagger')
     nltk.download('wordnet')
     nltk.download('stopwords')
-
+    nltk.download("averaged_perceptron_tagger_eng")
 
     # Add CSS Style
     with open('style.css') as f:
